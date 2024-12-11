35fa82fed04829d62eb17a41d3db6fcaf0f4ce1e
TDIR Admin <splunker>
2024-12-11 10:42:34 +0000

    testing

--- Phishing - Create Email Investigation.py
+++ Phishing - Create Email Investigation.py
@@ -84,7 +84,7 @@ def add_response_plan_1(action=None, success=None, container=None, results=None,
         if start_investigations_1_result_item[0] is not None:
             parameters.append({
                 "id": start_investigations_1_result_item[0],
-                "response_template_id": "70f636d1-e0ea-4d22-b51b-a4c86f655173",
+                "response_template_id": "7da1e2a7-4491-4479-8fdf-68bf9ea72f23",
                 "context": {'artifact_id': start_investigations_1_result_item[1]},
             })
 
@@ -121,39 +121,6 @@ def filter_1(action=None, success=None, container=None, results=None, handle=Non
     # call connected blocks if filtered artifacts or results
     if matched_artifacts_1 or matched_results_1:
         start_investigations_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
-
-    return
-
-
-@phantom.playbook_block()
-def refresh_finding_or_investigation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("refresh_finding_or_investigation_1() called")
-
-    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-
-    start_investigations_1_result_data = phantom.collect2(container=container, datapath=["start_investigations_1:action_result.data.*.id","start_investigations_1:action_result.parameter.context.artifact_id"], action_results=results)
-
-    parameters = []
-
-    # build parameters list for 'refresh_finding_or_investigation_1' call
-    for start_investigations_1_result_item in start_investigations_1_result_data:
-        if start_investigations_1_result_item[0] is not None:
-            parameters.append({
-                "id": start_investigations_1_result_item[0],
-                "context": {'artifact_id': start_investigations_1_result_item[1]},
-            })
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.act("refresh finding or investigation", parameters=parameters, name="refresh_finding_or_investigation_1", assets=["builtin_mc_connector"])
 
     return
 
@@ -347,87 +314,6 @@ def add_task_note_1(action=None, success=None, container=None, results=None, han
     ################################################################################
 
     phantom.act("add task note", parameters=parameters, name="add_task_note_1", assets=["builtin_mc_connector"], callback=create_event_1)
-
-    return
-
-
-@phantom.playbook_block()
-def get_response_plans_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("get_response_plans_1() called")
-
-    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-
-    parameters = []
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.act("get response plans", parameters=parameters, name="get_response_plans_1", assets=["builtin_mc_connector"], callback=filter_3)
-
-    return
-
-
-@phantom.playbook_block()
-def filter_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("filter_3() called")
-
-    # collect filtered artifact ids and results for 'if' condition 1
-    matched_artifacts_1, matched_results_1 = phantom.condition(
-        container=container,
-        conditions=[
-            ["get_response_plans_1:action_result.data.*.items.*.name", "==", "Phishing Email Investigation"]
-        ],
-        name="filter_3:condition_1",
-        delimiter=None)
-
-    # call connected blocks if filtered artifacts or results
-    if matched_artifacts_1 or matched_results_1:
-        debug_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
-
-    return
-
-
-@phantom.playbook_block()
-def debug_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("debug_1() called")
-
-    filtered_result_0_data_filter_3 = phantom.collect2(container=container, datapath=["filtered-data:filter_3:condition_1:get_response_plans_1:action_result.data.*.items.*.name"])
-
-    filtered_result_0_data___items___name = [item[0] for item in filtered_result_0_data_filter_3]
-
-    parameters = []
-
-    parameters.append({
-        "input_1": filtered_result_0_data___items___name,
-        "input_2": None,
-        "input_3": None,
-        "input_4": None,
-        "input_5": None,
-        "input_6": None,
-        "input_7": None,
-        "input_8": None,
-        "input_9": None,
-        "input_10": None,
-    })
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_1")
 
     return
 
